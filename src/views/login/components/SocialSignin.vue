e82a384e3b1075a67c7910c7279ab28d2d25be00
scriptfan <scriptfan@hotmail.com>
2026-01-19 10:48:07 +0800

    第三方登录控制

--- src/views/login/components/SocialSignin.vue
+++ src/views/login/components/SocialSignin.vue
@@ -1,72 +1,72 @@
 <template>
-  <div class="social-signup-container">
-    <div class="sign-btn" @click="wechatHandleClick('wechat')">
-      <span class="wx-svg-container"><svg-icon icon-class="wechat" class="icon" /></span>
-      WeChat
-    </div>
-    <div class="sign-btn" @click="tencentHandleClick('tencent')">
-      <span class="qq-svg-container"><svg-icon icon-class="qq" class="icon" /></span>
-      QQ
-    </div>
-  </div>
+  <el-row>
+    <el-col :span="24">
+      <el-button v-show="weixinLogin" type="primary" class="social-sign-btn" @click="weixinHandleClick()">
+        <span class="wx-svg-container"><svg-icon icon-class="wechat" class="icon" /></span>
+        微信登录
+      </el-button>
+    </el-col>
+    <el-col :span="24">
+      <el-button v-show="googleLogin" type="primary" class="social-sign-btn" @click="googleHandleClick()">
+        <span class="google-svg-container"><svg-icon icon-class="google" class="icon" /></span>
+        Google登录
+      </el-button>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-// import openWindow from '@/utils/open-window'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'SocialSignin',
+  data() {
+    return {
+      weixinLogin: false,
+      googleLogin: false
+    }
+  },
   methods: {
-    wechatHandleClick(thirdpart) {
+    weixinHandleClick() {
       alert('ok')
-      // this.$store.commit('SET_AUTH_TYPE', thirdpart)
-      // const appid = 'xxxxx'
-      // const redirect_uri = encodeURIComponent('xxx/redirect?redirect=' + window.location.origin + '/auth-redirect')
-      // const url = 'https://open.weixin.qq.com/connect/qrconnect?appid=' + appid + '&redirect_uri=' + redirect_uri + '&response_type=code&scope=snsapi_login#wechat_redirect'
-      // openWindow(url, thirdpart, 540, 540)
     },
-    tencentHandleClick(thirdpart) {
+    googleHandleClick() {
       alert('ok')
-      // this.$store.commit('SET_AUTH_TYPE', thirdpart)
-      // const client_id = 'xxxxx'
-      // const redirect_uri = encodeURIComponent('xxx/redirect?redirect=' + window.location.origin + '/auth-redirect')
-      // const url = 'https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirect_uri
-      // openWindow(url, thirdpart, 540, 540)
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'settings'
+    ])
+  },
+  watch: {
+    settings: {
+      immediate: true,
+      handler(newVal) {
+        if (newVal && newVal.socialitelogin) {
+          this.weixinLogin = newVal.socialitelogin.WeixinLogin
+          this.googleLogin = newVal.socialitelogin.GoogleLogin
+        }
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .social-signup-container {
-    margin: 20px 0;
-    .sign-btn {
-      display: inline-block;
-      cursor: pointer;
-    }
+  .social-sign-btn {
+    width: 100%;
+    margin-top: 15px;
+    justify-content: center;
     .icon {
       color: #fff;
-      font-size: 24px;
-      margin-top: 8px;
+      font-size: 16px;
     }
-    .wx-svg-container,
-    .qq-svg-container {
-      display: inline-block;
-      width: 40px;
-      height: 40px;
-      line-height: 40px;
-      text-align: center;
-      padding-top: 1px;
-      border-radius: 4px;
-      margin-bottom: 20px;
-      margin-right: 5px;
-    }
-    .wx-svg-container {
-      background-color: #24da70;
-    }
-    .qq-svg-container {
-      background-color: #6BA2D6;
-      margin-left: 50px;
-    }
+  }
+
+  .wx-svg-container,
+  .google-svg-container {
+    display: inline-block;
+    width: 16px;
   }
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -45,31 +45,10 @@
         </el-form-item>
       </el-tooltip>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button :loading="loading" type="primary" style="width:100%;" @click.native.prevent="handleLogin">Login</el-button>
 
-      <div style="position:relative">
-        <div class="tips">
-          <span>Username : admin</span>
-          <span>Password : any</span>
-        </div>
-        <div class="tips">
-          <span style="margin-right:18px;">Username : editor</span>
-          <span>Password : any</span>
-        </div>
-
-        <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
-          Or connect with
-        </el-button>
-      </div>
-    </el-form>
-
-    <el-dialog title="Or connect with" :visible.sync="showDialog">
-      Can not be simulated on local, so please combine you own business simulation! ! !
-      <br>
-      <br>
-      <br>
       <social-sign />
-    </el-dialog>
+    </el-form>
   </div>
 </template>
 
@@ -108,7 +87,6 @@ export default {
       passwordType: 'password',
       capsTooltip: false,
       loading: false,
-      showDialog: false,
       redirect: undefined,
       otherQuery: {}
     }
