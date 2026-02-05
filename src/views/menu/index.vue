02947a080b2664cbabadfe92ee911defbf72dfed
scriptfan <scriptfan@hotmail.com>
2026-02-05 15:36:08 +0800

    菜单管理优化

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -13,32 +13,62 @@
       :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
       border
     >
-      <el-table-column align="center" label="Name">
+      <el-table-column align="center" label="菜单名称">
         <template slot-scope="scope">
           {{ scope.row.name }}
         </template>
       </el-table-column>
-      <el-table-column align="center" label="Type">
+      <el-table-column align="center" label="菜单类型">
         <template slot-scope="scope">
-          {{ scope.row.menuType }}
+          <span
+            v-for="item in menuTypes"
+            :key="item.label"
+          >
+            <span v-if="scope.row.menuType === item.value">
+              <el-tag
+                v-if="scope.row.menuType === 1"
+                type=""
+                size="medium"
+                effect="dark"
+              >
+                目录
+              </el-tag>
+              <el-tag
+                v-else-if="scope.row.menuType === 2"
+                type="success"
+                size="medium"
+                effect="dark"
+              >
+                菜单
+              </el-tag>
+              <el-tag
+                v-else
+                type="warning"
+                size="medium"
+                effect="dark"
+              >
+                按钮
+              </el-tag>
+            </span>
+          </span>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="Permission">
+      <el-table-column align="center" label="权限名称">
         <template slot-scope="scope">
           {{ scope.row.permission }}
         </template>
       </el-table-column>
-      <el-table-column align="center" label="Route">
+      <el-table-column align="center" label="路由">
         <template slot-scope="scope">
           {{ scope.row.route }}
         </template>
       </el-table-column>
-      <el-table-column align="center" label="Component">
+      <el-table-column align="center" label="部件">
         <template slot-scope="scope">
           {{ scope.row.component }}
         </template>
       </el-table-column>
-      <el-table-column label="IsActive" min-width="100px">
+      <el-table-column label="启用" min-width="100px">
         <template slot-scope="{row}">
           <el-switch
             v-model="row.isActive"
@@ -48,13 +78,13 @@
           />
         </template>
       </el-table-column>
-      <el-table-column align="center" label="Actions" width="180" fixed="right">
+      <el-table-column align="center" label="操作" width="180" fixed="right">
         <template slot-scope="scope">
           <el-button type="primary" size="small" @click="handleUpdate(scope)">
-            Edit
+            编辑
           </el-button>
           <el-button type="danger" size="small" @click="handleDelete(scope)">
-            Delete
+            删除
           </el-button>
         </template>
       </el-table-column>
@@ -62,50 +92,50 @@
 
     <el-dialog :visible.sync="dialogVisible" :name="dialogType==='edit'?'Edit Menu':'New Menu'">
       <el-form :model="menu" label-width="120px" label-position="left">
-        <el-form-item label="Name">
-          <el-input v-model="menu.name" placeholder="Menu Name" />
-        </el-form-item>
-        <el-form-item label="Icon">
-          <el-input v-model="menu.icon" placeholder="Icon" />
-        </el-form-item>
-        <el-form-item label="Menu Type">
-          <el-input v-model="menu.menuType" placeholder="Menu Type" />
-        </el-form-item>
-        <el-form-item label="Route">
-          <el-input v-model="menu.route" placeholder="Route" />
-        </el-form-item>
-        <el-form-item label="Permission">
-          <el-input v-model="menu.permission" placeholder="Permission" />
-        </el-form-item>
-        <el-form-item label="Component">
-          <el-input v-model="menu.component" placeholder="Component" />
-        </el-form-item>
-        <el-form-item label="Parent">
-          <el-select v-model="menu.parentId" placeholder="Select Parent Menu">
+        <el-form-item label="菜单类型">
+          <el-select v-model="menu.menuType" placeholder="请选择">
             <el-option
-              v-for="item in list"
-              :key="item.id"
-              :label="item.name"
-              :value="item.id"
-            >
-              <el-tree
-                :data="[item]"
-                :props="{ value: 'id', label: 'name', children: 'children' }"
-                @node-click="(data) => { menu.parentId = data.id }"
-              />
-            </el-option>
+              v-for="item in menuTypes"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
           </el-select>
         </el-form-item>
-        <el-form-item label="IsActive" prop="isActive">
+        <el-form-item label="名称">
+          <el-input v-model="menu.name" placeholder="菜单名称" />
+        </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="menu.icon" placeholder="Icon" />
+        </el-form-item>
+        <el-form-item label="权限名">
+          <el-input v-model="menu.permission" placeholder="权限名" />
+        </el-form-item>
+        <el-form-item v-if="menu.menuType !== 3" label="路由">
+          <el-input v-model="menu.route" placeholder="路由" />
+        </el-form-item>
+        <el-form-item v-if="menu.menuType !== 3" label="部件路径">
+          <el-input v-model="menu.component" placeholder="部件路径" />
+        </el-form-item>
+        <el-form-item label="父菜单">
+          <el-cascader
+            v-model="menu.parentId"
+            :options="filteredTreeData"
+            :props="{ value: 'id', label: 'name', children: 'children', emitPath: false, checkStrictly: true }"
+            placeholder="Select Parent"
+            style="width: 100%"
+          />
+        </el-form-item>
+        <el-form-item label="启用" prop="isActive">
           <el-switch v-model="menu.isActive" />
         </el-form-item>
       </el-form>
       <div style="text-align:right;">
         <el-button type="danger" @click="dialogVisible=false">
-          Cancel
+          取消
         </el-button>
         <el-button type="primary" @click="confirmMenu">
-          Confirm
+          保存
         </el-button>
       </div>
     </el-dialog>
@@ -122,13 +152,57 @@ export default {
       list: [],
       dialogVisible: false,
       dialogType: 'new',
-      menu: {}
+      menu: {},
+      menuTypes: [
+        {
+          value: 1,
+          label: '目录'
+        },
+        {
+          value: 2,
+          label: '菜单'
+        },
+        {
+          value: 3,
+          label: '按钮'
+        }
+      ]
+    }
+  },
+  computed: {
+    // 过滤掉menuType为2的菜单项
+    filteredTreeData() {
+      if (!this.list || !Array.isArray(this.list)) {
+        return []
+      }
+      const menu = JSON.parse(JSON.stringify(this.list))
+      return this.filterMenuByType(menu, 3)
     }
   },
   created() {
     this.fetchMenuTree()
   },
   methods: {
+    // 根据菜单类型过滤菜单数据，递归过滤掉指定类型的菜单
+    filterMenuByType(data, excludeType) {
+      return data
+        .filter(item => item.menuType !== excludeType)
+        .map(item => {
+          if (item.children && Array.isArray(item.children)) {
+            const children = this.filterMenuByType(item.children, excludeType)
+            if (children && children.length > 0) {
+              return {
+                ...item,
+                children: children
+              }
+            } else {
+              delete item.children
+              return { ...item }
+            }
+          }
+          return item
+        })
+    },
     async fetchMenuTree() {
       const res = await getMenuTree()
       this.list = res.data.items
